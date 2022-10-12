53116f47d1abee39b5e9bbb8e8669480cba302b2
yerim <sontt4266@naver.com>
2022-10-13 04:08:01 +0900

    2_01_16 shared memory test

--- Chapter2/src/ch16/EmployeeTest.java
+++ Chapter2/src/ch16/EmployeeTest.java
@@ -3,9 +3,10 @@ package ch16;
 public class EmployeeTest {
 
 	public static void main(String[] args) {
-		// 실행해보면 두 인스턴스 다 ++가 되어있다. 이를 통해, 두 개의 인스턴스가 하나의 메모리를 공유하고 있음 알 수 있다.
+	// 실행해보면 두 인스턴스 다 ++가 되어있다. 
+	// 이를 통해, 두 개의 인스턴스가 하나의 메모리를 공유하고 있음을 알 수 있다.
 		
-		Employee employeeLee = new Employee();
+		Employee employeeLee = new Employee(); // Employee 로 선언된 지역변수 employeeLee
 		employeeLee.setEmployeeName("이토깽");
 		
 		System.out.println(employeeLee.serialNum);
@@ -19,3 +20,9 @@ public class EmployeeTest {
 	}
 
 }
+
+// [Memory]
+// Data 영역		- serialNum (static변수, 공유메모리)
+// Stack memory - employeeLee, employeeKim
+// Heap memory	- .setEmployeeName
+// Stack -> Data영역을 참조(공유메모리), Heap을 참조.
